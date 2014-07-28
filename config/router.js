035275c5ee3ef5efb1232348b34ff0be758335ba
Raven Lagrimas <rjlagrimas08@gmail.com>
2014-07-29 07:06:49 +0800

    Added get hackathon endpoint

--- config/router.js
+++ config/router.js
@@ -6,6 +6,7 @@ module.exports	= function (router, logger) {
 	router.del 	= router.delete;
 
 	router.get('/hackathons', hackathon.get_list);
+	router.get('/hackathon/:id', hackathon.get_by_id);
 
 
 	router.all('*', function (req, res, next) {
--- controllers/hackathon.js
+++ controllers/hackathon.js
@@ -19,15 +19,157 @@ exports.get_list = function (req, res, next) {
 				.sort({start_date : -1})
 				.toArray(send_response);
 		},
-		send_response = function (err, data) {
+		send_response = function (err, result) {
 			if (err) {
 				logger.log('warn', 'Error on getting list of hackathons');
 				return next(err);
 			}
 
 			logger.log('info', 'Successfully retrieved list of hackathons');
-			res.send(data);
+			res.send(result);
 		};
 
 	start();
 };
+
+exports.get_by_id = function (req, res, next) {
+	var data = {},
+		async_queue = 0,
+
+		start = function () {
+
+			logger.log('info', 'Getting hackathon with id', req.params.id);
+
+			mongo.collection('hackathons')
+				.findOne(
+					{_id : req.params.id},
+					{
+						portrait_image : 1,
+						name : 1,
+						description : 1,
+						theme : 1,
+						date : 1,
+						total_teams : 1,
+						external_link : 1,
+						judges : 1,
+						sponsors_and_partners : 1
+					},
+					get_judges_teams_sponsors
+				);
+		},
+		get_judges_teams_sponsors = function (err, result) {
+			var i;
+
+			if (err) {
+				logger.log('warn', 'Error getting hackathon');
+				return next(err);
+			}
+
+			data.hackathon = result;
+
+
+			logger.log('verbose', 'Getting judges');
+			async_queue++;
+			mongo.collection('judges')
+				.find(
+					{_id : {$in : result.judges}},
+					{
+						_id : 0,
+						name : 1
+					}
+				)
+				.toArray(function (err, result) {
+					data.hackathon.judges = result
+						.map(function (a) {
+							return a.name;
+						});
+					merge_async_calls(err, result);
+				});
+
+			logger.log('verbose', 'Getting sponsors and partners');
+			temp = result.sponsors_and_partners;
+			for (i in temp) {
+				(function (i) {
+					async_queue++;
+					mongo.collection('sponsors_and_partners')
+						.find(
+							{_id : {$in : temp[i]}},
+							{
+								_id : 0,
+								name : 1
+							})
+						.toArray(function (err, result) {
+							data.hackathon.sponsors_and_partners[i] = result
+								.map(function (a) {
+									return a.name;
+								});
+							merge_async_calls(err, result);
+						});
+				})(i);
+			}
+
+
+			logger.log('verbose', 'Getting winning teams');
+			async_queue++;
+			mongo.collection('teams')
+				.find({
+						hackathons : {$elemMatch : {hackathon_id : data.hackathon._id}},
+						'hackathons.awards' : {$exists : true}
+					},
+					{
+						'rank' : 1,
+						'hackers' : 1,
+						'hackathons.team_name' : 1,
+						'hackathons.app_name' : 1,
+						'hackathons.points' : 1,
+						'hackathons.awards' : 1,
+					})
+				.sort({'hackathons.points' : 1})
+				.toArray(function (err, result) {
+					data.teams = result;
+					merge_async_calls(err, result);
+				});
+		},
+		merge_async_calls = function (err, result) {
+			if (err) {
+				logger.log('warn', 'Error getting things');
+				return next(err);
+			}
+
+			if (--async_queue === 0) {
+				logger.log('verbose', 'Getting winning team hackers');
+
+				data.teams.forEach(function (a, i) {
+					async_queue++;
+					mongo.collection('hackers')
+						.find(
+							{_id : {$in : a.hackers}},
+							{
+								_id : 0,
+								name : 1,
+								image : 1,
+								username : 1
+							}
+						)
+						.toArray(function (err, result) {
+							data.teams[i].hackers = result;
+							send_response(err, result);
+						});
+				});
+			}
+
+		},
+		send_response = function (err, result) {
+			if (err) {
+				logger.log('warn', 'Error getting hackers');
+				return next(err);
+			}
+
+			if (--async_queue === 0) {
+				res.send(data);
+			}
+		};
+
+	start();
+};
+
